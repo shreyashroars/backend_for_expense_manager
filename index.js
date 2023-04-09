5ef84c601560991ff2a565e9c5853e63f7483c6d
Shreyash Pandey <shreyash.edu@gmail.com>
2023-04-09 17:00:13 +0530

    refactoring vercel file

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express =require('express');
 const app = express();
 const txRouter = require('./Routers/txRouter');
+app.get('/',(req,res)=>
+{
+    res.send("Main Page");
+
+});
 app.use(
     express.urlencoded({ extended: true })
 );
+
 app.use(express.json()); 
 app.listen(3000);
 app.use('/transaction',txRouter);
